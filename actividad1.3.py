0a091abb261323824263d4ea452531997de9c714
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-09-27 17:14:40 +0200

    sugundo commit

--- actividad1.3.py
+++ actividad1.3.py
@@ -1,21 +1,20 @@
 import os
 import csv
-# Crear un archivo y escribir en el
+# Crear un archivo y escribir en el los datos
 file = open("clientes_secuencial.txt", "w")
-file.write("12345678,Joan,Gonzalez,654321789\n" + "87654321,Ana,Martinez,987654321\n" + "13579246,Lluís,Rodriguez,564738291\n" + "24681357,Maria,Perez,987654765")
+file.write("12345678,Joan,Gonzalez,654321789\n" + "87654321,Ana,Martinez,987654321\n" + "13579246,Lluis,Rodriguez,564738291\n" + "24681357,Maria,Perez,987654765")
 file.close()
 
-# f = open("clientes_secuencial.txt", "r")
-with open("clientes_secuencial", "r") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=",")
-    
-# for i in range(4):
-#     texto = f.readline()
-#     if "2" in texto:
-#         print("Se ha encontrado al usuario " + texto)
-#     else :
-#         print("No se ha encontrado al usuario")
 
-
-# f.close()
-
+# Lee los datos del archivo y hace una busquedad por DNI
+dni = "87654321"
+with open("clientes_secuencial.txt", "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        if(row[0] == dni):
+            print("Cliente encontrado:")
+            for data in row:
+                print(data)
+            break
+        else: 
+            print("Cliente no encontrado")
