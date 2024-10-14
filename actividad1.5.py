2a3a80dd596c925ff6d65bed7b53fea118d560f2
AlvaroQP <alonsol1569@gmail.com>
2024-10-14 12:05:47 +0200

    Solo falta el menu

--- actividad1.5.py
+++ actividad1.5.py
@@ -10,13 +10,6 @@ else:
 
 # --------------Funciones------------
 # Lista los empleados del fichero empleados1.5.txt
-
-def leerFichero():
-    with open("empleados1_5.txt", "r") as csv_file:
-        csv_reader = csv.reader(csv_file)
-
-    
-
 def listarEmpleados():
     with open("empleados1_5.txt", "r") as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -26,35 +19,42 @@ def listarEmpleados():
             print("Salario: " + row[3] + "\n")
 
 
-def modificarSalario(nombre):
+def agregarEmpleado():
+    datos = list()
+    datos.append(input("Dime el Nombre: "))
+    datos.append(input("Dime el Apellido: "))
+    datos.append(input("Dime el Cargo: "))
+    datos.append(input("Dime el Salario: "))
+    with open("empleados1_5.txt", "a") as f:
+        f.write("\n" +datos[0]+ "," + datos[1] + "," + datos[2] + "," + datos[3])
+    print("Empleado creado correctamente")
+
+def modificarSalario(nombre, nuevoSalario):
     empleado = list()
+    empleados = list()
     with open("empleados1_5.txt", "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
-            if(nombre == row[0]):
+            if nombre in row:
                 empleado = row
+            else:
+                empleados.append(row)
 
-    print(empleado)
+    # Si el empleado existe hace los cambios
+    if empleado:
+        empleado[3] = str(nuevoSalario)
+        empleados.append(empleado)
+        # Reescribe el archivo original y hace los cambios 
+        with open("empleados1_5.txt", "w") as file:
+            for em in empleados:
+                file.write(",".join(em) + "\n")
+    else:
+        print("No se ha encontrado al empleado")
+
+    print(empleados)
 # listarEmpleados()
-modificarSalario("Juan")
-
-# Agrega un nuevo cliente y lo guarda en el fichero
-# def agregarCliente():
-#     datos = list()
-#     print("Dime el dni")
-#     datos.append(input())
-#     print("Dime el nombre")
-#     datos.append(input())
-#     print("Dime el apellido")
-#     datos.append(input())
-#     print("Dime el telefono")
-#     datos.append(input())
-
-#     f = open("clientes_secuencial.txt", "a")
-#     f.write("\n" + datos[0]+ "," + datos[1] + "," + datos[2] + "," + datos[3])
-#     f.close()
-#     print("Cliente creado")
-#     print(datos[0]+ "," + datos[1] + "," + datos[2] + "," + datos[3])
+# modificarSalario("Juan", 3000)
+agregarEmpleado()
 
 
 # ----------------Main----------------
