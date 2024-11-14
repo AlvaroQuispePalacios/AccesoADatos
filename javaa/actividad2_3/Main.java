6eb53c327839ee9f9395eb3987e3499d832254d1
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-11-14 19:04:28 +0100

    Actividad 2.3 hecha

--- javaa/actividad2_3/Main.java
+++ javaa/actividad2_3/Main.java
@@ -8,46 +8,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        CrearFichero();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dima la palabra a buscar");
+        System.out.println("Dime la palabra a buscar");
         String palabraABuscar = sc.nextLine();
-
+        BuscarPalabra(palabraABuscar);
     }
 
-    public void BuscarPalabra(String palabra) {
+    public static void BuscarPalabra(String palabraABuscar) {
         try {
             FileReader fr = new FileReader("./fichero23.txt");
             BufferedReader br = new BufferedReader(fr);
             String linea = br.readLine();
             while (linea != null) {
-                if (linea.contains(palabra)) {
-                    
+                if (linea.contains(palabraABuscar)) {
+                    System.out.println(linea + "\n");
+                    String[] palabras = linea.split(" ");
+                    for (int i = 0; i < palabras.length; i++) {
+                        if (palabras[i].equals(palabraABuscar)) {
+                            System.out.println(i);
+
+                        }
+                    }
                 }
+                linea = br.readLine();
             }
             br.close();
         } catch (Exception ex) {
-
-        }finally{
-
+            System.out.println(ex.getMessage());
         }
     }
 
-    public void CrearFichero() {
+    public static void CrearFichero() {
         try {
             File fichero23 = new File("./fichero23.txt");
             if (!fichero23.exists()) {
                 String data = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet mollitia fuga ea enim impedit quis nemo? Veniam amet tempore neque velit quis magnam, placeat voluptatibus porro ut, sit debitis! Cumque!\r\n"
-                + //
-                "    Voluptates laudantium dolores omnis numquam rerum ducimus amet! Corporis numquam, sit deleniti vitae magnam est explicabo earum excepturi molestias ipsa mollitia modi eum fugiat debitis quos, vero error iste consequuntur.\r\n"
-                + //
-                "    Rem esse doloremque rerum ipsam nemo debitis, excepturi aperiam, officia fuga mollitia natus possimus odio maxime at tempora ullam ut saepe magni deserunt placeat minus. Nobis, debitis. Eligendi, nihil sunt?";
+                        + //
+                        "    Voluptates laudantium dolores omnis numquam rerum ducimus amet! Corporis numquam, sit deleniti vitae magnam est explicabo earum excepturi molestias ipsa mollitia modi eum fugiat debitis quos, vero error iste consequuntur.\r\n"
+                        + //
+                        "    Rem esse doloremque rerum ipsam nemo debitis, excepturi aperiam, officia fuga mollitia natus possimus odio maxime at tempora ullam ut saepe magni deserunt placeat minus. Nobis, debitis. Eligendi, nihil sunt?";
                 fichero23.createNewFile();
                 FileWriter fw = new FileWriter(fichero23);
                 fw.write(data);
                 fw.close();
                 System.out.println("Fichero creado");
             }
-        } catch (Exception e) {
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
 
         }
 
