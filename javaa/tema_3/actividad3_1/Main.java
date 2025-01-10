3d37e5e36348d24338395828a92cb9dcadb30fb0
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2025-01-10 16:11:28 +0100

    10-01-2025

--- javaa/tema_3/actividad3_1/Main.java
+++ javaa/tema_3/actividad3_1/Main.java
@@ -11,17 +11,17 @@ public class Main {
         try {
             Connection con = DriverManager.getConnection(url, "root", "");
             Statement s = con.createStatement();
-            // s.execute("CREATE TABLE clientes(dni VARCHAR(9) NOT NULL PRIMARY KEY,apellidos VARCHAR(32) NOT NULL,cp VARCHAR(5))");
-            // int nFil = s.executeUpdate("INSERT INTO clientes(dni, apellidos, cp) \r\n" + //
-            //                     "VALUES\r\n" + //
-            //                     "('78901234X', 'NADALES', '44126'),\r\n" + //
-            //                     "('89012345E', 'HOJAS', null),\r\n" + //
-            //                     "('56789012B', 'SAMPER', '29730'),\r\n" + //
-            //                     "('09876543K', 'LAMIQUIE', null)\r\n" + //
-            //                     "");
+            s.execute("CREATE TABLE clientes(dni VARCHAR(9) NOT NULL PRIMARY KEY,apellidos VARCHAR(32) NOT NULL,cp VARCHAR(5))");
+            int nFil = s.executeUpdate("INSERT INTO clientes(dni, apellidos, cp) \r\n" + //
+                                "VALUES\r\n" + //
+                                "('78901234X', 'NADALES', '44126'),\r\n" + //
+                                "('89012345E', 'HOJAS', null),\r\n" + //
+                                "('56789012B', 'SAMPER', '29730'),\r\n" + //
+                                "('09876543K', 'LAMIQUIE', null)\r\n" + //
+                                "");
 
 
-            // System.out.println(nFil + " Filas insertadas");
+            System.out.println(nFil + " Filas insertadas");
             s.executeUpdate("DELETE FROM clientes WHERE dni='09876543K'");
             s.close();
             con.close();
